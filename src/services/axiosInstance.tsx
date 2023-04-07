34f3892c3070add61287bc2ed373d33625719d1f
timmy471 <imosioluwa@gmail.com>
2023-04-07 12:52:25 +0100

    put api base url in code - env is in gitignore

--- src/services/axiosInstance.tsx
+++ src/services/axiosInstance.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_API_VARIABLE;
+// const baseURL = process.env.NEXT_PUBLIC_API_VARIABLE;
+const baseURL = 'http://localhost:9002/api/';
 
 const axiosInstance = axios.create({
   baseURL,
